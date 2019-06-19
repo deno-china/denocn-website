3f453dbfd838dc03f6f348506d5b6792423cfd72
Enok <416828041@qq.com>
2019-06-19 09:55:23 +0800

    添加SEO优化路径指向

--- dev.js
+++ dev.js
@@ -1,13 +1,19 @@
 const ParcelProxyServer = require("parcel-proxy-server");
+
+const targetHost = "http://127.0.0.1:3000";
+
 const server = new ParcelProxyServer({
   entryPoint: "./src/index.html",
   parcelOptions: {},
   proxies: {
     "/api": {
-      target: "http://127.0.0.1:3000",
+      target: targetHost,
       pathRewrite: {
         "^/api": "/"
       }
+    },
+    "/seo": {
+      target: targetHost
     }
   }
 });
