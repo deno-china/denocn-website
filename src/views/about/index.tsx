6f8c092933ec26cac3f82484e9a630632085dbb4
lingzi <1101077695@qq.com>
2019-07-02 00:10:05 +0800

    关于页面

--- src/views/about/index.tsx
+++ src/views/about/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import DefaultLayout from '../../components/layouts/default';
 import './index.less';
+import MarkdownPreview from '../../components/markdown-preview';
 
 export default class About extends DefaultLayout {
+  componentWillMount() {
+    fetch('http://127.0.0.1:1234/about.md').then(res => res.text()).then(text => this.setState({ content: text }));
+  }
+
+  state = { content: '' };
+
   renderContent(): JSX.Element {
-    return <div>About</div>;
+    return <MarkdownPreview content={this.state.content} />;
   }
 
   renderSide(): JSX.Element {
