b9c20a72f641a8dcffa25563527c5e8b03215876
steve xu <stevexugc@gmail.com>
2019-07-14 14:18:28 +0800

    feat: Add Reply

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,9 +1,30 @@
+import { ReplyDetail } from '../store/detail';
+
 const DEFAULT_TITLE = 'Deno中文社区';
 
-export function setDocumentTitle(title: string = '') {
+export function setDocumentTitle(title: string = ''): void {
+  if (!document) {
+    return;
+  }
   if (title) {
     document.title = `${title} - ${DEFAULT_TITLE}`;
   } else {
     document.title = DEFAULT_TITLE;
   }
+}
+
+// 解决用户名可能为空的问题
+export function getUserName(item: ReplyDetail): string {
+  return `${item.author_nick_name || item.author_id}`;
+}
+
+// 回复的用户名转换为链接
+export function getRealConent(content: string, flagMap: any): string {
+  return content.replace(/@\S+/g, (key: string): string => {
+    const link = flagMap[key];
+    if (link) {
+      return `[${key}](${link})`;
+    }
+    return key;
+  });
 }
--- src/pages/detail/_add-reply.tsx
+++ src/pages/detail/_add-reply.tsx
@@ -4,70 +4,96 @@ import {
   faInfoCircle,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { FunctionComponent, useCallback, useState } from 'react';
+import { observer } from 'mobx-react';
+import React, { Component } from 'react';
 import { message, Spin } from 'antd';
 import { httpPost } from '../../common/request';
 import Editor from '../../components/editor';
 import BasePanel from '../../components/panels/base-panel';
-import detailStore from '../../store/detail';
 import './_add-reply.less';
+import replyStore from '../../store/reply';
+import detailStore from '../../store/detail';
 
 interface AddReplyProps {
   topicId: number;
   replyTo?: number;
 }
 
-const AddReply: FunctionComponent<AddReplyProps> = ({ topicId, replyTo }) => {
-  const [content, setContent] = useState('');
-  const [loading, setLoading] = useState(false);
-  const onCommit = useCallback(async () => {
+interface AddReplyState {
+  loading: boolean;
+}
+
+@observer
+class AddReply extends Component<AddReplyProps, AddReplyState> {
+  state = {
+    loading: false,
+  };
+
+  handleClick = () => {
+    const { topicId, replyTo } = this.props;
+    const { content } = replyStore;
     if (!content.trim().length) {
       message.error('请输入内容');
       return;
     }
-    setLoading(true);
-    try {
-      await httpPost('/api/reply/add', {
-        content,
-        topic_id: topicId,
-        reply_to: replyTo,
+    this.setState({
+      loading: true,
+    });
+    httpPost('/api/reply/add', {
+      content,
+      topic_id: topicId,
+      reply_to: replyTo,
+    }).then(() => {
+      this.setState({
+        loading: false,
       });
-    } finally {
-      setLoading(false);
-    }
-    setContent('');
-    detailStore.load(topicId);
-  }, [content]);
-  return (
-    <Spin spinning={loading}>
-      <BasePanel
-        white
-        className="panel-detail-add-reply"
-        header={(
-          <>
-            <div>
-              <FontAwesomeIcon
-                icon={faCommentDots}
-                style={{ marginRight: 10 }}
-              />
-              发表回复
-            </div>
-            <button className="btn green" onClick={onCommit}>
-              <FontAwesomeIcon icon={faCheck} style={{ marginRight: 10 }} />
-              提交
-            </button>
-          </>
-        )}
-      >
-        <div className="tips">
-          <FontAwesomeIcon icon={faInfoCircle} />
-          可以粘贴上传图片
-        </div>
+      replyStore.setContent('');
+      detailStore.load(topicId);
+    }).finally(() => {
+      this.setState({
+        loading: false,
+      });
+    });
+  };
 
-        <Editor value={content} onChange={setContent} />
-      </BasePanel>
-    </Spin>
-  );
-};
+  handleChange = (value: string) => {
+    replyStore.setContent(value);
+  };
+
+  render() {
+    const { loading } = this.state;
+    const { content } = replyStore;
+    return (
+      <Spin spinning={loading}>
+        <BasePanel
+          white
+          className="panel-detail-add-reply"
+          header={(
+            <>
+              <div>
+                <FontAwesomeIcon
+                  icon={faCommentDots}
+                  style={{ marginRight: 10 }}
+                />
+                发表回复
+              </div>
+              <button className="btn green" onClick={this.handleClick}>
+                <FontAwesomeIcon icon={faCheck} style={{ marginRight: 10 }} />
+                提交
+              </button>
+            </>
+          )}
+        >
+          <div className="tips">
+            <FontAwesomeIcon icon={faInfoCircle} />
+            可以粘贴上传图片
+          </div>
+
+          <Editor value={content} onChange={this.handleChange} />
+        </BasePanel>
+      </Spin>
+    );
+  }
+}
 
 export default AddReply;
